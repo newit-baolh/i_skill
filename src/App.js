29ae653733e311acc8da027f6f37b15a7eeaacdf
newit-baolh <baolh@newit.co.jp>
2022-04-03 19:38:17 +0700

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Button } from 'reactstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Learn React</h1>
+      <Button color='danger'>ReactStrap</Button>
     </div>
   );
 }
